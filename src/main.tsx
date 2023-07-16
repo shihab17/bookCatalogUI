8a5f9c89c8f136c98780ea7191e1634149ef2239
shihab17 <shihabahmed620@gmail.com>
2023-07-16 23:02:04 +0600

    redux configure

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './routes/index.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/index.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </React.StrictMode>
+);
